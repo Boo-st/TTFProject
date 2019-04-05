fba1c0ea7a676e8099f1f78003aca4e309829dba
mitchel <mitchel_king@icloud.com>
2019-04-05 22:05:08 +0800

    add working init.py

--- flaskdb/venv/init.py
+++ flaskdb/venv/init.py
@@ -7,27 +7,14 @@ app = Flask(__name__)
 
 @app.route('/ttf')
 def db():
-	try:
-		conn = sqlite3.connect('/home/pi/TTFProject/flaskdb/venv/TTF.db')
-		c = conn.cursor()
-		data = c.execute('SELECT * FROM weather;')
-		clean = data.fetchall()
-		return(render_template(index.html, weather=clean))
-		conn.close()
-	except Exception, e:
-		return str(e)
-
-def db2():
-		try:
-		conn = sqlite3.connect('/home/pi/TTFProject/flaskdb/venv/TTF.db')
-		c = conn.cursor()
-		data = c.execute('SELECT * FROM userData;')
-		clean = data.fetchall()
-		return(render_template(index.html, info=clean))
-		conn.close()
-	except Exception, e:
-		return str(e)
-
+	conn = sqlite3.connect('/home/pi/TTFProject/flaskdb/venv/TTF.db')
+	c = conn.cursor()
+#	countRows = c.rowcount
+	data = c.execute('SELECT * FROM weather;')
+#	for row in data:
+	clean = data.fetchall()
+	return(render_template('index.html', item=clean))
+#	return(clean)
 if __name__ == "__main__":
 	app.run()
 
